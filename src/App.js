29527592a681ece2e701fee7b2024e08f7bef56d
marcos melgar <marcosjmelgar@gmail.com>
2020-12-13 22:45:21 -0300

    add todo fixed

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 import Header from './components/layout/Header';
 import Todos from './components/Todos';
 import AddTodo from './components/AddTodo';
-
+//import uuid from 'uuid';
+//import axios from 'axios';
 
 import './App.css';
 
 class App extends Component {
-
   state = {
     todos: [
       {
@@ -17,58 +17,76 @@ class App extends Component {
       },
       {
         id: 2,
-      title: "take in",
-      completed: true
+        title: "take in",
+        completed: false
       },
       {
         id: 3,
-        title: "holas",
+        title: "take up",
         completed: true
       }
     ]
-  }
+  };
+
+  /*componentDidMount() {
+    axios
+      .get('https://jsonplaceholder.typicode.com/todos?_limit=10')
+      .then(res => this.setState({ todos: res.data }));
+  }*/
 
   // Toggle Complete
-  markComplete = (id) => {
-    this.setState({ todos: this.state.todos.map(todo => {
-      if(todo.id === id) {
-        todo.completed = !todo.completed
-      }
-      console.log(todo)
-      return todo;
-    })});
-  }
+  markComplete = id => {
+    this.setState({
+      todos: this.state.todos.map(todo => {
+        if (todo.id === id) {
+          todo.completed = !todo.completed;
+        }
+        return todo;
+      })
+    });
+  };
 
   // Delete Todo
-  delTodo = (id) => {
-    this.setState({ todos: [...this.state.todos.filter(todo => todo.id !==
-      id)]});
-      console.log(id)
-  }
-
+  delTodo = id => {
+      this.setState({
+        todos: [...this.state.todos.filter(todo => todo.id !== id)]});
+  };
 
   // Add Todo
-    addTodo = (title) => {
-      const newTodo = {
+
+  addTodo = title => {
+    const newTodo = {
       id: 8,
       title,
       completed: true
     }
     this.setState({ todos: [...this.state.todos, newTodo]})
-    console.log(title)
   }
-
+  /*addTodo = title => {
+    axios
+      .post('https://jsonplaceholder.typicode.com/todos', {
+        title,
+        completed: false
+      })
+      .then(res => {
+        res.data.id = uuid.v4();
+        this.setState({ todos: [...this.state.todos, res.data] });
+      });
+  };
+  */
   render() {
-    console.log(this.state.todos)
     return (
-      <div className="App">
-        <div className= "container">
-          <Header />
-          <AddTodo addTodo={this.addTodo} />
-          <Todos todos={this.state.todos} markComplete={this.markComplete} 
-          delTodo={this.delTodo}/>
+        <div className="App">
+          <div className="container">
+            <Header />
+                <AddTodo addTodo={this.addTodo} />
+                <Todos
+                  todos={this.state.todos}
+                  markComplete={this.markComplete}
+                  delTodo={this.delTodo}
+                />
+            </div>
         </div>
-      </div>
     );
   }
 }
--- src/components/Todos.js
+++ src/components/Todos.js
@@ -3,18 +3,18 @@ import TodoItem from './TodoItem';
 import PropTypes from 'prop-types';
 
 class Todos extends Component {
-
   render() {
-    return this.props.todos.map((todo => (
-        <TodoItem key= {todo.id} todo={todo} markComplete=
-        {this.props.markComplete} delTodo={this.props.delTodo} />
-    )));
+    return this.props.todos.map((todo) => (
+      <TodoItem key={todo.id} todo={todo} markComplete={this.props.markComplete} delTodo={this.props.delTodo} />
+    ));
   }
 }
 
 // PropTypes
 Todos.propTypes = {
-    todos : PropTypes.array.isRequired
+  todos: PropTypes.array.isRequired,
+  markComplete: PropTypes.func.isRequired,
+  delTodo: PropTypes.func.isRequired,
 }
 
 export default Todos;
